b773369a067c3cd93786f5d3702b8a69714e94d4
Libby <ecosedge@gmail.com>
2025-07-14 13:38:52 +0100

    blergh

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+// app/providers.tsx
+'use client';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
+  cache: new InMemoryCache(),
+});
+
+export function ApolloProviders({ children }: { children: React.ReactNode }) {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
