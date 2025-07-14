a9ca9798e18a3639295d4da8dbb96dc4d8c9a4b5
Libby <ecosedge@gmail.com>
2025-07-14 18:14:48 +0100

    wip starting to get to fleshing out

---
+++ app/listings/page.tsx
@@ -0,0 +1,9 @@
+export default function ListingsPage() {
+  return (
+    <div>
+      <h1>Listings Page</h1>
+      <p>This is the listings page.</p>
+      {/* You can add more components or content here */}
+    </div>
+  );
+}
